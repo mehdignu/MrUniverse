a8eaf9de4ab9fdd8cb5ba5a24508e22638bb50c2
mehdi dridi <mehdignu@users.noreply.github.com>
2018-01-08 22:59:16 +0100

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		//we want to move 0.0 for up and down the others are user movements of right and left
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement;
+	} 
+}﻿
+
