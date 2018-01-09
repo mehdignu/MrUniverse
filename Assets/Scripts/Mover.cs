1fbf2367bc6e03041cfd2047ef9e0f388c5f2024
mehdi dridi <mehdignu@users.noreply.github.com>
2018-01-09 14:36:05 +0100

    ded

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+	public GameObject shot;
+
+	public float speed;
+	public Rigidbody rb;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3 (0.0f, 0.0f, speed);
+
+	}
+
+
+
+
+
+}﻿
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+
 public class PlayerController : MonoBehaviour 
 {
-	public Rigidbody rb;
 
-	void Start() 
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn; 
+	public float fireRate;
+
+	private float nextFire; 
+
+	void Update () 
 	{
-		rb = GetComponent<Rigidbody> ();
+
+		//fire1 is preset value in the game manager
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+
+			//we want to instantiate the bolt Object
+			shot = Instantiate(shot) as GameObject;
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			shot.transform.position = shotSpawn.transform.position;
+
+		}
 	}
-
-	void FixedUpdate () 
+	void FixedUpdate ()
 	{
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		//we want to move 0.0 for up and down the others are user movements of right and left
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.velocity = movement;
-	} 
-}﻿
+		GetComponent<Rigidbody>().velocity = movement * speed;
 
+		GetComponent<Rigidbody>().position = new Vector3
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+
+}﻿
